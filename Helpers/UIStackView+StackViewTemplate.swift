191ce0b1ee881a0f67113ed92e08114bfcc58422
Marlon Raskin <marlonjraskin@gmail.com>
2020-01-25 19:35:22 -0500

    Added StackView helper

--- Helpers/UIStackView+StackViewTemplate.swift
+++ Helpers/UIStackView+StackViewTemplate.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Marlon Raskin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+	static func fillStackView(axis: NSLayoutConstraint.Axis = .vertical, spacing: CGFloat, with views: [UIView]) -> UIStackView {
+		let stackView = UIStackView()
+		stackView.axis = axis
+		stackView.spacing = spacing
+		stackView.alignment = .fill
+		stackView.distribution = .fill
+		views.forEach { stackView.addArrangedSubview($0) }
+		return stackView
+	}
+}
