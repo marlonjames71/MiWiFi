09890d4b7eb156195483baf7c37b289fd50195ea
Marlon Raskin <marlonjraskin@gmail.com>
2020-01-26 02:10:22 -0500

    Added wifi init, tweaked infoview constraints
    Adjusted BG color, added isFavorite indicator in NavBar

--- View Controllers/WIFIDetailVC.swift
+++ View Controllers/WIFIDetailVC.swift
@@ -11,29 +11,46 @@ import UIKit
 class WIFIDetailVC: UIViewController {
 
 	var wifiController: WifiController?
-	var wifi: Wifi? {
-		didSet {
-			updateViews()
-		}
+
+	let wifi: Wifi
+
+	let qrImageView: MiWiFiImageView
+
+	init(with wifi: Wifi) {
+		self.wifi = wifi
+		self.qrImageView = MiWiFiImageView(with: wifi)
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		fatalError("Use init(with wifi: Wifi)")
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
 	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		view.backgroundColor = .systemBackground
 		configureNavController()
-		updateViews()
+		configureView()
+		configureWifiInfoView()
     }
 
-	private func updateViews() {
-		guard let wifi = wifi else { return }
-		title = wifi.name
-
-		let qrImageView = MiWiFiImageView(with: wifi)
+	private func configureView() {
+		view.backgroundColor = .miBackground
 		layoutImageView(imageView: qrImageView)
 	}
 
 	private func configureNavController() {
-		title = "WiFi Name"
+		title = wifi.name
+		let star = UIImage(systemName: "star.fill")?.withTintColor(.systemOrange, renderingMode: .alwaysOriginal)
+		let starImageView = UIImageView(image: star)
+
+		if wifi.isFavorite {
+			navigationItem.titleView = starImageView
+		}
+
 		let editButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTapped(_:)))
 		editButton.tintColor = .miTintColor
 		navigationItem.rightBarButtonItem = editButton
@@ -42,15 +59,25 @@ class WIFIDetailVC: UIViewController {
 	private func layoutImageView(imageView: UIImageView) {
 		view.addSubview(imageView)
 		NSLayoutConstraint.activate([
-			imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+			imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
 			imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
 			imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
 			imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
 		])
 	}
 
+	private func configureWifiInfoView() {
+		let infoView = WiFiInfoView(with: wifi)
+
+		view.addSubview(infoView)
+		NSLayoutConstraint.activate([
+			infoView.topAnchor.constraint(equalTo: qrImageView.bottomAnchor, constant: 30),
+			infoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+			infoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+		])
+	}
+
 	@objc private func editButtonTapped(_ sender: UIBarButtonItem) {
-		guard let wifi = wifi else { return }
 		let editVC = AddWIFIVC()
 		editVC.wifi = wifi
 		editVC.modalPresentationStyle = .overFullScreen
