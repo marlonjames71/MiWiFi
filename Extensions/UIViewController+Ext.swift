d0c5665e1f7b605b5d004073b62b9c3b32be20af
Marlon <marmoneymar@icloud.com>
2020-01-29 03:09:45 -0500

    Added UIViewController extension
    Might use for Alerts

---
+++ Extensions/UIViewController+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+Ext.swift
+//  MiWiFi
+//
+//  Created by Marlon Raskin on 1/28/20.
+//  Copyright © 2020 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+	
+	func showEmptyStateView(with message: String, in view: UIView) {
+		let emptyStateView = MiWiFiEmptyStateView(message: message)
+		emptyStateView.frame = view.bounds
+		view.addSubview(emptyStateView)
+	}
+}
