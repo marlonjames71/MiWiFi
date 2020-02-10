15f8f0a77438d3b8cf4c6f57099e83a5217e9686
Marlon Raskin <marlonjraskin@gmail.com>
2020-02-10 01:00:36 -0500

    Fixed spacing

--- MiWiFi/Views/ImageViews/MiWiFiIconImageView.swift
+++ MiWiFi/Views/ImageViews/MiWiFiIconImageView.swift
@@ -15,20 +15,24 @@ class MiWiFiIconImageView: UIImageView {
 		case password = "shield.lefthalf.fill"
 	}
 
+
 	var icon: Icon? {                                                                                                                                                                                                                       
 		didSet {
 			configure()
 		}
 	}
 
+
 	override init(frame: CGRect = .zero) {
 		super.init(frame: frame)
 		configure()
 	}
 
+
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+
 
 	private func configure() {
 		translatesAutoresizingMaskIntoConstraints = false
