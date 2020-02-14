960fc744d89d308779f6e38d10b97eb82c3a3efc
Marlon <marmoneymar@icloud.com>
2020-02-14 02:33:57 -0500

    Updated Colors. Added ToolBar class for MultiSelect
    ToolBar not implemented yet.

---
+++ MiWiFi/Views/Custom Views/MiToolBar.swift
@@ -0,0 +1,21 @@
+//
+//  MiiToolBar.swift
+//  MiWiFi
+//
+//  Created by Marlon Raskin on 2/12/20.
+//  Copyright © 2020 Marlon Raskin. All rights reserved.
+//
+
+import UIKit
+
+class MiToolBar: UIToolbar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
