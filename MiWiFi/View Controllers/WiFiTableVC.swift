e459a1bb1129d7c0f1736e86efad59c942c82a72
Marlon <marmoneymar@icloud.com>
2020-03-13 18:03:01 -0400

    (Fixed but still janky) cell update with animation from context menu

--- MiWiFi/View Controllers/WiFiTableVC.swift
+++ MiWiFi/View Controllers/WiFiTableVC.swift
@@ -278,39 +278,8 @@ extension WiFiTableVC: UITableViewDelegate, UITableViewDataSource {
 
 	func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
 		let wifi = self.fetchedResultsController.object(at: indexPath)
-		let nickname = wifi.nickname ?? ""
 
-		let configuration = UIContextMenuConfiguration(identifier: wifi.menuID, previewProvider: { () -> UIViewController? in
-			return WIFIDetailVC(with: wifi)
-		}) { action in
-
-			let favoriteStr = wifi.isFavorite ? "Unfavorite" : "Favorite"
-			let favStar = UIImage(systemName: "star")
-			let unfavStar = UIImage(systemName: "star.fill")?.withTintColor(.miFavoriteTint, renderingMode: .alwaysOriginal)
-			let starImage = wifi.isFavorite ? unfavStar : favStar
-
-			let favorite = UIAction(title: favoriteStr, image: starImage) { action in
-				let cell = tableView.cellForRow(at: indexPath) as? MiWiFiCell
-				DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
-					WifiController.shared.updateFavorite(wifi: wifi, isFavorite: !wifi.isFavorite)
-					let indexPath = self.wifiTableView.indexPath(for: cell ?? UITableViewCell())
-					let newCell = self.wifiTableView.cellForRow(at: indexPath!) as! MiWiFiCell
-					newCell.updateViews()
-				}
-			}
-
-			let deleteAction = UIAction(title: "Delete", image: UIImage(systemName: "trash.fill"), attributes: .destructive) { UIAction in
-				DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
-					WifiController.shared.delete(wifi: wifi)
-				}
-			}
-
-			let deleteMenu = UIMenu(title: "Delete", image: UIImage(systemName: "trash.fill"), options: .destructive, children: [deleteAction])
-
-			return UIMenu(title: "WiFi: \(nickname)", identifier: UIMenu.Identifier(rawValue: "favorite"), children: [favorite, deleteMenu])
-		}
-
-		return configuration
+		return UIContextMenuConfiguration.newWiFitableViewConfiguration(wifi: wifi, delegate: self, indexPath: indexPath)
 	}
 
 
@@ -393,10 +362,14 @@ extension WiFiTableVC: NSFetchedResultsControllerDelegate {
 			guard let newIndexPath = newIndexPath else { return }
 			wifiTableView.insertRows(at: [newIndexPath], with: .automatic)
 		case .move:
+			print("Move")
 			guard let indexPath = indexPath,
 				let newIndexPath = newIndexPath else { return }
+			let cell = wifiTableView.cellForRow(at: indexPath) as? MiWiFiCell
+			cell?.updateViews()
 			wifiTableView.moveRow(at: indexPath, to: newIndexPath)
 		case .update:
+			print("Update")
 			guard let indexPath = indexPath else { return }
 			wifiTableView.reloadRows(at: [indexPath], with: .automatic)
 		case .delete:
@@ -405,5 +378,46 @@ extension WiFiTableVC: NSFetchedResultsControllerDelegate {
 		@unknown default:
 			print(#line, #file, "unknown NSFetchedResultsChangeType: \(type)")
 		}
+	}
+}
+
+
+extension WiFiTableVC: WiFiTableViewConfigurationDelegate {
+	func didUpdateFavorite(wifi: Wifi, indexPath: IndexPath) {
+		WifiController.shared.updateFavorite(wifi: wifi, isFavorite: !wifi.isFavorite)
+	}
+}
+
+
+protocol WiFiTableViewConfigurationDelegate: class {
+	func didUpdateFavorite(wifi: Wifi, indexPath: IndexPath)
+}
+
+extension UIContextMenuConfiguration {
+	class func newWiFitableViewConfiguration(wifi: Wifi, delegate: WiFiTableViewConfigurationDelegate, indexPath: IndexPath) -> UIContextMenuConfiguration {
+
+		return UIContextMenuConfiguration(identifier: wifi.menuID, previewProvider: { () -> UIViewController? in
+			return WIFIDetailVC(with: wifi)
+		}, actionProvider: { action in
+
+			let favoriteStr = wifi.isFavorite ? "Unfavorite" : "Favorite"
+			let favStar = UIImage(systemName: "star")
+			let unfavStar = UIImage(systemName: "star.fill")?.withTintColor(.miFavoriteTint, renderingMode: .alwaysOriginal)
+			let starImage = wifi.isFavorite ? unfavStar : favStar
+
+			let favorite = UIAction(title: favoriteStr, image: starImage) { action in
+				delegate.didUpdateFavorite(wifi: wifi, indexPath: indexPath)
+			}
+
+			let deleteAction = UIAction(title: "Delete", image: UIImage(systemName: "trash.fill"), attributes: .destructive) { UIAction in
+				DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+					WifiController.shared.delete(wifi: wifi)
+				}
+			}
+
+			let deleteMenu = UIMenu(title: "Delete", image: UIImage(systemName: "trash.fill"), options: .destructive, children: [deleteAction])
+
+			return UIMenu(title: "WiFi: \(wifi.nickname ?? "")", identifier: UIMenu.Identifier(rawValue: "favorite"), children: [favorite, deleteMenu])
+		})
 	}
 }
