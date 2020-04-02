825fd2ba0349da209013d25db23c1817dc09f79f
Marlon <marmoneymar@icloud.com>
2020-04-01 22:06:48 -0400

    (Added) User defaults for FaceID Setting

--- MiWiFi/Utils/Defaults.swift
+++ MiWiFi/Utils/Defaults.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+fileprivate extension String {
+	static let defaultsVersion = "com.augustlight.miwifi.defaultversion"
+	static let faceIDEnabled = "FaceIDEnabled"
+}
+
+enum DefaultsManager {
+	fileprivate static let defaults = UserDefaults.standard
+
+	static var faceIDEnabled: Bool {
+		get {
+			defaults.object(forKey: .faceIDEnabled) as? Bool ?? true
+		}
+		set {
+			defaults.set(newValue, forKey: .faceIDEnabled)
+		}
+	}
+
+	static var defaultsVersion: Int {
+		get {
+			defaults.integer(forKey: .defaultsVersion)
+		}
+		set {
+			defaults.set(newValue, forKey: .defaultsVersion)
+		}
+	}
+
+	static func migrateDefaults() {
+		if defaultsVersion == 0 {
+			defaults.set(1, forKey: String.defaultsVersion)
+		}
+	}
+}
+
+extension UserDefaults {
+	subscript(key: String) -> Any? {
+		get {
+			object(forKey: key)
+		}
+		set {
+			set(newValue, forKey: key)
+		}
+	}
+
+	subscript(bool key: String) -> Bool {
+		get {
+			bool(forKey: key)
+		}
+		set {
+			set(newValue, forKey: key)
+		}
+	}
+}
--- MiWiFi/View Controllers/WiFiSettingsVC.swift
+++ MiWiFi/View Controllers/WiFiSettingsVC.swift
@@ -34,8 +34,16 @@ class WiFiSettingsVC: UIViewController, WiFiSettingsVCDelegate {
 
 	]
 
-	private let creditArr: [Settings] = [Settings(title: "Acknowledgements", iconName: "qrcode",
-												  color: .miAddButtonColor, renderingMode: .alwaysOriginal,
+	private let settingsArr: [Settings] = [Settings(title: "FaceID",
+													iconName: "faceid",
+													color: .systemBlue,
+													renderingMode: .alwaysOriginal,
+													config: .standardConfiguration)]
+
+	private let creditArr: [Settings] = [Settings(title: "Acknowledgements",
+												  iconName: "qrcode",
+												  color: .miAddButtonColor,
+												  renderingMode: .alwaysOriginal,
 												  config: .standardConfiguration)]
 
 	private var indexPath: IndexPath?
@@ -83,9 +91,8 @@ class WiFiSettingsVC: UIViewController, WiFiSettingsVCDelegate {
 
 		miSettingsTableView.delegate = self
 		miSettingsTableView.dataSource = self
-		miSettingsTableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+		miSettingsTableView.register(SettingsCell.self, forCellReuseIdentifier: reuseIdentifier)
 	}
-
 }
 
 
@@ -128,15 +135,17 @@ extension WiFiSettingsVC: MFMailComposeViewControllerDelegate {
 
 extension WiFiSettingsVC: UITableViewDelegate, UITableViewDataSource {
 	func numberOfSections(in tableView: UITableView) -> Int {
-		return 2
+		return 3
 	}
 
 
 	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 		switch section {
 		case 0:
-			return "Contact & Share"
+			return "Security"
 		case 1:
+			return "Contact & Share"
+		case 2:
 			return "Acknowledgements"
 		default:
 			break
@@ -148,8 +157,10 @@ extension WiFiSettingsVC: UITableViewDelegate, UITableViewDataSource {
 	func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
 		switch section {
 		case 0:
-			return "Contact the developer if you have an idea or spot a bug."
+			return "Having FaceID enabled ensures that your WiFi passwords are safe. Your passwords are only stored on your device."
 		case 1:
+			return "Contact the developer if you have an idea or spot a bug."
+		case 2:
 			return "Credits for 3rd party technology used to make the app even better."
 		default:
 			break
@@ -161,8 +172,10 @@ extension WiFiSettingsVC: UITableViewDelegate, UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		switch section {
 		case 0:
-			return contactArr.count
+			return settingsArr.count
 		case 1:
+			return contactArr.count
+		case 2:
 			return creditArr.count
 		default:
 			break
@@ -172,17 +185,23 @@ extension WiFiSettingsVC: UITableViewDelegate, UITableViewDataSource {
 	}
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? SettingsCell else { return UITableViewCell() }
 		cell.backgroundColor = .miSecondaryBackground
 		cell.accessoryType = .disclosureIndicator
 
 		switch indexPath.section {
 		case 0:
+			cell.textLabel?.text = settingsArr[indexPath.row].title
+			cell.imageView?.image = settingsArr[indexPath.row].iconImage
+			cell.accessoryType = .none
+			cell.shouldHideSwitch = false
+		case 1:
 			cell.textLabel?.text = contactArr[indexPath.row].title
 			cell.imageView?.image = contactArr[indexPath.row].iconImage
-		case 1:
+		case 2:
 			cell.textLabel?.text = creditArr[indexPath.row].title
 			cell.imageView?.image = creditArr[indexPath.row].iconImage
+
 		default:
 			break
 		}
@@ -193,6 +212,8 @@ extension WiFiSettingsVC: UITableViewDelegate, UITableViewDataSource {
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		switch indexPath.section {
 		case 0:
+			tableView.deselectRow(at: indexPath, animated: true)
+		case 1:
 			switch indexPath.row {
 			case 0:
 				shareApp()
@@ -223,17 +244,29 @@ extension WiFiSettingsVC: UITableViewDelegate, UITableViewDataSource {
 			default:
 				break
 			}
-		case 1:
+		case 2:
 			let acknowledgementVC = MiWiFiAcknowledgmentVC()
 			acknowledgementVC.delegate = self
 			acknowledgementVC.modalPresentationStyle = .overFullScreen
 			acknowledgementVC.modalTransitionStyle = .crossDissolve
 			present(acknowledgementVC, animated: true)
+
 		default:
 			break
 		}
 
 		self.indexPath = indexPath
 //		tableView.deselectRow(at: indexPath, animated: true)
+	}
+}
+
+
+extension WiFiSettingsVC: FaceIDAlertDelegate {
+	func showPasswordRequestedFailed() {
+		self.presentFailedVerificationWithFaceIDAlert()
+	}
+
+	func biometricAuthenticationNotAvailable() {
+		self.presentBiometryNotAvailableAlert()
 	}
 }
